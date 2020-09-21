01a257748b3e578d285de95727dbb05b954b552a
BasilJohn <ferdenend@gmail.com>
2020-09-21 16:11:40 +0530

    Updated testcases & code refractoring

--- __tests__/GNavigationButtonComponent-test.js
+++ __tests__/GNavigationButtonComponent-test.js
@@ -5,10 +5,35 @@
 import 'react-native';
 import React from 'react';
 // Note: test renderer must be required after react-native.
+import { shallow } from 'enzyme';
+import { fireEvent } from '@testing-library/react-native';
 import renderer from 'react-test-renderer';
 import { GNavigationButtonComponent } from '../src/customComponent';
 
-test('Renders Button Component Correctly', () => {
+test('Renders GNavigationButtonComponent Component Correctly', () => {
   const tree = renderer.create(<GNavigationButtonComponent />).toJSON();
   expect(tree).toMatchSnapshot();
+});
+
+it('Check prop value is being set', () => {
+  const wrap = shallow(
+    <GNavigationButtonComponent />
+  );
+  wrap.setProps({ title: 'Go to Welcome Screen' });
+  expect(wrap.find('Text').props().children).toEqual('Go to Welcome Screen');
+});
+
+it('Click event', () => {
+  const navProps = {
+    navigation: {
+      navigate: jest.fn(),
+    }
+  };
+  const wrap = shallow(
+    <GNavigationButtonComponent navProps={navProps} />
+  );
+  const buttonElemment = wrap.props().children;
+  fireEvent(buttonElemment, 'onPress', 'middle');
+  expect(navProps.navigation.navigate).toHaveBeenCalled();
+  expect(navProps.navigation.navigate).toHaveBeenCalledTimes(1);
 });
--- __tests__/GValidatorComponent-test.js
+++ __tests__/GValidatorComponent-test.js
@@ -6,6 +6,7 @@ import 'react-native';
 import React from 'react';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
 import { GValidatorComponent } from '../src/customComponent';
 
 const operation = jest.fn();
@@ -13,4 +14,12 @@ const operation = jest.fn();
 test('Renders GButtonComponent Component Correctly', () => {
   const tree = renderer.create(<GValidatorComponent operation={operation} />).toJSON();
   expect(tree).toMatchSnapshot();
+});
+
+it('Check prop value is being set', () => {
+  const wrap = shallow(
+    <GValidatorComponent operation={operation} />
+  );
+  wrap.setProps({ final: 'Combined Object' });
+  expect(wrap.find('Text').getElements()[4].props.children).toEqual('Combined Object');
 });
