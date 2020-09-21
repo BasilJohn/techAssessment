5d2445d20c845edce01538a2258ac59f5bef7a2c
BasilJohn <ferdenend@gmail.com>
2020-09-21 16:20:10 +0530

    Fix Flow Issues

--- src/utils/hoc/buttonHOCComponent.js
+++ src/utils/hoc/buttonHOCComponent.js
@@ -1,6 +1,11 @@
+/**
+ * @format
+ * @flow strict-local
+ */
 import React from 'react';
 
-export const higherOrderComponent = (WrappedComponent, containerStyle, textStyle) => {
+export const higherOrderComponent = (WrappedComponent:any,
+  containerStyle:Object, textStyle:Object) => {
   const buttonHOC = () => {
     return <WrappedComponent containerStyle={containerStyle} textStyle={textStyle} />;
   };
